ee0d89b96be49e268ac53cee20a973d4c8096a8d
Dennis Gülich <dennis.gulich@gmail.com>
2016-11-07 17:46:41 +0100

    install larger cask packages one at the time

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -19,11 +19,14 @@ apps=(
   charles
   skype
   docker
-  intellij-idea
-  libreoffice
 )
 
 brew cask install "${apps[@]}"
+
+# Install larger packages one at the time
+
+brew cask install intellij-idea
+brew cask install libreoffice
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize
